e20ae2932c545024f97fb458b85f166a337fb564
nathanquinteiro <nathan.quinteiro@gmail.com>
2017-06-18 21:01:13 +0200

    Correct Farenheit Celsius conversion

--- app.py
+++ app.py
@@ -74,16 +74,16 @@ def makeWebhookResult(data):
         return {}
 
     # print(json.dumps(item, indent=4))
-    print ("Temp in farenheit: ")
+    print ("Temp in Farenheit: ")
     print (condition.get('temp'))
 
-    tempF = int(condition.get('temp'))
-    print (tempF)
-
-    temperature = (temp_f - 32) * (5.0 / 9.0)
+    tempF = int("33")
+    tempC = int((tempF - 32) * (5.0 / 9.0) + 0.5)
+    print ("Temp in Celsius: ")
+    print (tempC)
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + temperature + " " + "°C"
+             ", the temperature is " + tempC + " " + "°C"
 
     print("Response:")
     print(speech)
@@ -121,6 +121,7 @@ def makeWebhookResult(data):
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
+
 
     print("Starting app on port %d" % port)
 
